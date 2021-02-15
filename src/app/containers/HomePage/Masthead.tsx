dda36fba6edef4a7b05015ef4d523d50c93f2ae7
amitkurud <amit.kurud@gmail.com>
2021-02-15 16:26:19 +0530

    Style Changes and Index page grammar fix

--- src/app/containers/HomePage/Masthead.tsx
+++ src/app/containers/HomePage/Masthead.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components/macro';
 import { Logos } from './Logos';
 import { Title } from 'app/containers/HomePage/components/Title';
 import { Lead } from 'app/containers/HomePage/components/Lead';
+import tw from 'twin.macro';
 
 export function Masthead() {
+  const [counter, setCounter] = useState(0);
   return (
     <Wrapper>
       <Logos />
+      {/*
       <Title>Amit Kurud</Title>
+*/}
       <Lead>
-        Hi there, my name is Amit Kurud. I am a Software Engineer located in
+        Hello there
+        <span role="img" aria-label="hand-wave">
+          👋
+        </span>
+        , my name is <strong> Amit Kurud</strong>. I am a Software Engineer
+        located in
         <strong> Hyderabad, India</strong>. I work as a{' '}
-        <strong> Fullstack Software Engineer</strong> . I program in a variety
-        of languages but most of the time I use
+        <strong> Fullstack Software Engineer</strong>. I program in a variety of
+        languages but most of the time I use
         <strong> Java</strong> to accomplish my task. I also work with{' '}
         <strong>
           {' '}
@@ -23,9 +32,17 @@ export function Masthead() {
         . My focus is on writing clean and maintainable software. I'm very eager
         to learn, in my free time I read about various topics ranging from
         writing clean code to specific frameworks/programming languages. I also
-        contribute to the opensource community in our organisation and on
-        Github, run a linux desktop at home and love to chat about tech in
+        contribute to the opensource community in our organization and on
+        Github, run a Linux desktop at home, and love to chat about tech in
         general.
+        <br />
+        <P title="Github Page">
+          <strong>
+            <a href="blog" rel="noopener noreferrer">
+              Please Feel Free to Read My Blog
+            </a>
+          </strong>
+        </P>
       </Lead>
     </Wrapper>
   );
@@ -39,4 +56,9 @@ const Wrapper = styled.main`
   justify-content: center;
   min-height: 320px;
   margin-bottom: 2rem;
+`;
+const P = styled.p`
+  font-size: 1.25rem;
+  color: ${p => p.theme.text};
+  ${tw`text-center font-sans text-2xl`}
 `;
