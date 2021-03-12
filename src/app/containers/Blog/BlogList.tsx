b86083f99c4015a5bf49ead0749cf710f8c6aec5
amitkurud <amit.kurud@gmail.com>
2021-03-12 11:23:10 +0530

    Removing Unnecessary Changes

--- src/app/containers/Blog/BlogList.tsx
+++ src/app/containers/Blog/BlogList.tsx
@@ -1,16 +1,24 @@
-import { Title } from '../HomePage/components/Title';
 import React from 'react';
 import styled from 'styled-components/macro';
 
 export function BlogList() {
   return (
     <>
-      <Title>Coming Soon</Title>
+      <Center>Coming Soon</Center>
       <List></List>
     </>
   );
 }
+
+
 const List = styled.ul`
   padding: 0;
   margin: 6.25rem 0 0 0;
+`;
+const Center = styled.h1`
+  font-size: 32px;
+  font-weight: bold;
+  color: ${p => p.theme.text};
+  margin: 1rem 0;
+  text-align: center;
 `;
